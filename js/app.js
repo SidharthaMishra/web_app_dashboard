010bbf5c82a0f28c669c3643e59702f6ea37b78c
Sidhartha Mishra <SidharthaMishra@users.noreply.github.com>
2020-11-06 19:13:29 -0500

    Adds notifications dropdown functionality for hide/show

--- js/app.js
+++ js/app.js
@@ -1,17 +1,46 @@
-const searchIcon = document.querySelector(".search");
-const mobileSearchBox = document.querySelector(".mobile-search-box>input");
+document.addEventListener("DOMContentLoaded", function() {
+    const searchIcon = document.querySelector(".search");
+    const mobileSearchBox = document.querySelector(".mobile-search-box>input");
+    const bell = document.querySelector(".bell");
+    const notificationsDropdown = document.querySelector(".notifications-dropdown");
 
-searchIcon.addEventListener("click", function() {
+    //Event Listeners
+    searchIcon.addEventListener("click", function() {
 
-    if (mobileSearchBox.classList.contains("hidden")) {
-        mobileSearchBox.classList.add("slideIn");
-        mobileSearchBox.classList.remove("hidden");
-    } else if (mobileSearchBox.classList.contains("slideIn")) {
-        mobileSearchBox.classList.remove("slideIn");
-        mobileSearchBox.classList.add("slideOut");
-    } else if (mobileSearchBox.classList.contains("slideOut")) {
-        mobileSearchBox.classList.remove("slideOut");
-        mobileSearchBox.classList.add("slideIn");
-    }
+        if (mobileSearchBox.classList.contains("hidden")) {
+            mobileSearchBox.classList.add("slideIn");
+            mobileSearchBox.classList.remove("hidden");
+        } else if (mobileSearchBox.classList.contains("slideIn")) {
+            mobileSearchBox.classList.remove("slideIn");
+            mobileSearchBox.classList.add("slideOut");
+        } else if (mobileSearchBox.classList.contains("slideOut")) {
+            mobileSearchBox.classList.remove("slideOut");
+            mobileSearchBox.classList.add("slideIn");
+        }
+
+    });
+
+    bell.addEventListener("click", function() {
+
+        if (notificationsDropdown.classList.contains("hidden")) {
+            if (notificationsDropdown.classList.contains("lift")) {
+                notificationsDropdown.classList.remove("lift");
+            }
+            notificationsDropdown.classList.add("drop");
+            notificationsDropdown.classList.remove("hidden");
+
+        } else {
+            if (notificationsDropdown.classList.contains("drop")) {
+                notificationsDropdown.classList.remove("drop");
+                notificationsDropdown.classList.add("lift");
+                setTimeout(function() {
+                    notificationsDropdown.classList.add("hidden");
+                }, 200);
+            }
+        }
+
+    });
+
+
 
 });
